075ea774007085a1557447b2bff39c5fbf5d8b4d
ikromovsardor <ikromovs545@gmai.com>
2021-08-10 21:09:04 +0300

    assas

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,24 @@
-const gulp        = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const gulp   = require('gulp');
+const sass   = require('gulp-sass')(require('sass'));
 const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
-// Static server
 
-
-
-gulp.task("styles", function () {
+gulp.task('styles',function () {
     return gulp.src("src/sass/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe (rename({prefix: "", suffix: ".min"}))
+        .pipe(rename({prefix: "", suffix: ".min"}))
         .pipe(autoprefixer({
-            cascade: false
+            browsers :['last 2 versions'],
+              cascade: false
         }))
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("scr/css"));
+        .pipe(gulp.dest("src/css"))
 })
 
-gulp.task("watch", function () {
-    gulp.watch("src/sass/*.+(scss|sass",gulp.parallel("styles"));
-})
+gulp.task('watch',function () {
+   gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
+});
 
-gulp.task("default", gulp.parallel("watch", "styles"));
+gulp.task('default', gulp.parallel('watch', 'styles'));
